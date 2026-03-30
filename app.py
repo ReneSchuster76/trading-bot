43e51cf7c75161e97bfc3c0967ea28ce3f9492f5
ReneSchuster76 <gzero01@web.de>
2026-03-30 02:15:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def send_telegram_message(message: str) -> None:
         print("Telegram ENV fehlt.")
         return
 
-url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {
         "chat_id": TELEGRAM_CHAT_ID,
         "text": message
